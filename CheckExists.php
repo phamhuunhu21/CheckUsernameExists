0fd753697eed2818cd654617bc37d7d30567c3ff
Nhu Pham <phamhuunhu21@gmail.com>
2017-09-27 11:18:28 +0700

    Fix khong de trong username

--- CheckExists.php
+++ CheckExists.php
@@ -4,15 +4,21 @@
 	$rowCount = 0;
 	$uname = $_POST["txtUname"];
 
-	$qr = "SELECT * FROM users WHERE username = '$uname'";
-	if ($result = mysqli_query($con, $qr)){
-		$rowCount = mysqli_num_rows($result);
-	}
-
-	if ($rowCount > 0) {
-		echo "Username đã tồn tại";
+	if ($uname == "") {
+		echo "Vui lòng không để trống";
 	}
 	else {
-		echo "Bạn có thể dùng Username này để đăng ký";
+		$qr = "SELECT * FROM users WHERE username = '$uname'";
+		if ($result = mysqli_query($con, $qr)){
+			$rowCount = mysqli_num_rows($result);
+		}
+
+		if ($rowCount > 0) {
+			echo "Username đã tồn tại";
+		}
+		else {
+			echo "Bạn có thể dùng Username này để đăng ký";
+		}
 	}
+	
  ?>
